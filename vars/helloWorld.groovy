0ffbddc6900d0ce159438b84f9ab72f05852b1e6
Surya Kiran P <48705134+surya3796@users.noreply.github.com>
2023-10-14 21:55:45 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,11 @@
 def call() {
-    // Your logic here...
-    bat "echo Hello World"
+    if (isUnix()) {
+        sh "echo Hello World"
+    } else {
+        bat "echo Hello World"
+    }
+}
+
+boolean isUnix() {
+    return System.getProperty("os.name").toLowerCase().contains("linux") || System.getProperty("os.name").toLowerCase().contains("mac")
 }

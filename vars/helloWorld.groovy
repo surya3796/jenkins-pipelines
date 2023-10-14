e32f432fcc7a78db51bcaa16bb32458c770cbe35
Surya Kiran P <48705134+surya3796@users.noreply.github.com>
2023-10-14 21:52:32 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,11 +1,4 @@
 def call() {
-    if (isUnix()) {
-        sh "echo Hello World"
-    } else {
-        bat "echo Hello World"
-    }
-}
-
-boolean isUnix() {
-    return sh(script: 'exit 0', returnStatus: true) == 0
+    // Your logic here...
+    bat "echo Hello World"
 }
